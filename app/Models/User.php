5a0740295ebc88de8ec6b87466d78fad443bcba7
Vasili Raminashvili <vaska.r02@gmail.com>
2024-07-15 23:00:57 +0400

    fix user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
